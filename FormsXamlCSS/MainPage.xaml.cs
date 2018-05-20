374eb6f238972904453131fde892b1c9722a2c6b
“subbu.balaraju@gmail.com” <subbu.balaraju@gmail.comgit config --global user.name SubramanyamRajuBgit config --global user.email subbu.balaraju@gmail.com>
2018-05-20 12:49:37 +0530

    - Xamarin.Forms CSS Sample

---
+++ FormsXamlCSS/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Reflection;
+using Xamarin.Forms;
+using Xamarin.Forms.StyleSheets;
+
+namespace FormsXamlCSS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //this.Resources.Add(StyleSheet.FromAssemblyResource(
+                //IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly,
+                //"FormsXamlCSS.Assets.styles.css"));
+
+            //using (var reader = new StringReader("^contentpage { background-color: lightgray; }"))
+            //{
+            //    this.Resources.Add(StyleSheet.FromReader(reader));
+            //}
+        }
+    }
+}

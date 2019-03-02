2c2f206a077237a99e7aa5f6df51c15949a4dd30
Subramanyam Balaraju <subbu.balaraju@gmail.com>
2019-03-02 11:51:56 +0530

    - Added HtmlParsing article source code.

---
+++ HtmlParsing/HtmlParsing/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Xamarin.Forms;
+
+namespace HtmlParsing
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            string html = @"<!DOCTYPE html>  
+<html>  
+<body>  
+<h2>HTML Image Parsing</h2>  
+<img src='https://www.w3schools.com/html/img_girl.jpg' alt='Girl in a jacket' width='500' height='600'>  
+</body>  
+</html>  ";
+            myImage.Source = HtmlParsing(html);
+        }
+
+        string HtmlParsing(string html)
+        {
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+            //Getting img src value    
+            var imgUrl = document.DocumentNode.Descendants("img").FirstOrDefault().GetAttributeValue("src", null);
+            return imgUrl;
+        }
+    }
+}

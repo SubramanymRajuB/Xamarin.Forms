6d613591f702b839d72c10109489a255ae1d1877
Subramanyam Balaraju <subbu.balaraju@gmail.com>
2019-03-10 10:59:28 +0530

    - Added Local Json Parsing article source code.

---
+++ LocalJsonParsing/LocalJsonParsing/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Reflection;
+using LocalJsonParsing.Model;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace LocalJsonParsing
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetJsonData();
+        }
+
+        void GetJsonData()
+        {
+            ContactList ObjContactList = new ContactList();
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.contacts.json");
+            using (var reader = new System.IO.StreamReader(stream))
+            {
+                var jsonString = reader.ReadToEnd();
+                //Converting JSON Array Objects into generic list  
+                ObjContactList = JsonConvert.DeserializeObject<ContactList>(jsonString);
+            }
+            //Binding listview with server response    
+            listviewConacts.ItemsSource = ObjContactList.contacts;
+        }
+    }
+
+}
+
---
+++ RestDemo/RestDemo/RestDemo/Views/JsonParsingPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using RestDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RestDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class JsonParsingPage : ContentPage
+    {
+        public JsonParsingPage()
+        {
+            InitializeComponent();
+            GetJSON();
+        }
+        public async void GetJSON()
+        {
+            //Check network status 
+            if (NetworkCheck.IsInternet())
+            {
+             
+                var client = new System.Net.Http.HttpClient();
+                var response = await client.GetAsync("http://api.androidhive.info/contacts/");
+                string contactsJson = response.Content.ReadAsStringAsync().Result;
+                ContactList ObjContactList = new ContactList();
+                if (contactsJson != "")
+                {
+                    //Converting JSON Array Objects into generic list
+                    ObjContactList = JsonConvert.DeserializeObject<ContactList>(contactsJson);
+                }
+                //Binding listview with server response  
+                listviewConacts.ItemsSource = ObjContactList.contacts;
+            }
+            else
+            {
+                await DisplayAlert("JSONParsing", "No network is available.", "Ok");
+            }
+            //Hide loader after server response  
+            ProgressLoader.IsVisible = false;
+        }
+        
+        private void listviewContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var itemSelectedData = e.SelectedItem as Contact;
+            Navigation.PushAsync(new JsonDetailsPage(itemSelectedData));
+        }
+    }
+}

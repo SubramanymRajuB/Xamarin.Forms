db1408ec6ffe9ca69a2aa4a01bad213fd431ccdf
Subramanyam Balaraju <subbu.balaraju@gmail.com>
2019-03-10 11:35:17 +0530

    - Optimizing code

--- LocalJsonParsing/LocalJsonParsing/MainPage.xaml.cs
+++ LocalJsonParsing/LocalJsonParsing/MainPage.xaml.cs
@@ -16,16 +16,18 @@ namespace LocalJsonParsing
 
         void GetJsonData()
         {
+            string jsonFileName = "contacts.json";
             ContactList ObjContactList = new ContactList();
             var assembly = typeof(MainPage).GetTypeInfo().Assembly;
-            Stream stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.contacts.json");
+            Stream stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.{jsonFileName}");
             using (var reader = new System.IO.StreamReader(stream))
             {
                 var jsonString = reader.ReadToEnd();
+
                 //Converting JSON Array Objects into generic list  
                 ObjContactList = JsonConvert.DeserializeObject<ContactList>(jsonString);
             }
-            //Binding listview with server response    
+            //Binding listview with json string   
             listviewConacts.ItemsSource = ObjContactList.contacts;
         }
     }

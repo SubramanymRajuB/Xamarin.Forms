babc98472c9afc1d80f69433b3345a8b62c1d8ac
Subramanyam Balaraju <subbu.balaraju@gmail.com>
2019-04-26 11:36:14 +0530

    - Added Azure blob storage source code.

---
+++ AzureStorage/FileUploader/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace FileUploader
+{
+    public static class Constants
+    {
+        public const string StorageConnection = "DefaultEndpointsProtocol=https;AccountName=xamarinazurestorage;AccountKey=8MFvZNQ4e42KuKJ0uC1JyMWhTg8r1QhOpRU5IQUsxL+QBweqv7jkESBOC6J2mN20T4dtm6m32fDnumdPl/uFpg==;EndpointSuffix=core.windows.net";
+        //public const string StorageConnection = "UseDevelopmentStorage=true"; // Uncomment this connection string to use the Azure Storage emulator
+    }
+}

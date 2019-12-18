753a616177e3cb954fc4e68175a162e6ad22eff4
balarsu <subramanyam.balaraju@lamresearch.com>
2019-12-18 12:15:18 +0530

    - Added DateFormat sample

---
+++ DatePickerFromat/DatePickerFromat.Android/Renderers/CustomDatePicker.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(DatePicker))]
+namespace DatePickerFromat.Droid.Renderers
+{
+    public class CustomDatePicker : DatePickerRenderer
+    {
+        public CustomDatePicker(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null && Control != null)
+            {// creating gradient drawable for the curved background  
+                var _gradientBackground = new GradientDrawable();
+                _gradientBackground.SetShape(ShapeType.Rectangle);
+
+                //_gradientBackground.SetColor(Android.Graphics.Color.Gray);
+
+                // Thickness of the stroke line  
+                _gradientBackground.SetStroke(1, Android.Graphics.Color.Gray);
+
+                // Radius for the curves  
+                _gradientBackground.SetCornerRadius(10);
+
+                // set the background of the   
+                Control.SetBackground(_gradientBackground);
+
+                this.Control.Text = Element.Date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)?.Replace("-", "/");
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (Element != null && Control != null)
+            {
+                this.Control.Text = Element.Date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)?.Replace("-", "/");
+            }
+        }
+    }
+}
+
---
+++ DatePickerFromat/DatePickerFromat.iOS/Renderers/CustomDatePicker.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Globalization;
+using DatePickerFromat.iOS.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(DatePicker), typeof(CustomDatePicker))]
+namespace DatePickerFromat.iOS.Renderers
+{
+    public class CustomDatePicker : DatePickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<DatePicker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && Control != null)
+            {
+                this.Control.Text = Element.Date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)?.Replace("-", "/");
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (Element != null && Control != null)
+            {
+                this.Control.Text = Element.Date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture)?.Replace("-", "/");
+            }
+        }
+
+    }
+}
